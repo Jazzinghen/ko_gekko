f3f687176b28164dec6ef410a2fa862d79b93e06
Jazzinghen <jazzinghen@gmail.com>
2023-01-25 19:31:49 +0100

    Added comments to Dockerfile and runner

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,21 @@
 # syntax=docker/dockerfile:1
 FROM python:3.10-bullseye
 
+# Disable root warnings for PIP and use Pipenv in folders
 ENV PIPENV_VENV_IN_PROJECT 1
 ENV PIP_ROOT_USER_ACTION ignore
 
-# Install environment tools to handle
+# Install environment tools to handle.
+# It's not super good practice to not provide versions when using pip install,
+# but this is a "meta-call" since we are upgrading pip to the latest version
+# and installing the most recent pipenv for security reasons
 RUN pip install --no-cache-dir --upgrade pip && \
     pip install --no-cache-dir pipenv
 
+# Create environment
 RUN ["mkdir", "-p", "/opt/ko_gekko/downloads"]
 WORKDIR /opt/ko_gekko
 
+# Setup application
 COPY . .
 RUN pipenv install
--- ko_gekko.sh
+++ ko_gekko.sh
@@ -3,27 +3,34 @@
 # Functions
 function cleanup {
     echo "Removing temporary data..."
-    #rm -rf "${out_dir}"
+    # This command might fail in case of user namespace redirection.
+    rm -rf "${out_dir}" 2>/dev/null
     popd >/dev/null || exit
 }
 
 # Main
+# Find the actual location of the script and go there
 runner_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 pushd "${runner_dir}" >/dev/null || exit
 
 trap cleanup EXIT
 
+# Create a temporary directory to store all the output from the container and
+# set the permissions to read/write for everyone in case of user namespace
+# redirection
 out_dir="$(mktemp -d --suffix=kogekko)"
 chmod 777 "${out_dir}"
 
-KOGEKKO_DOWNLOADS="${out_dir}" docker compose run --rm ko_gekko pipenv run python src/main.py "$@"
+# Actually call the application in docker passing all the remaining arguments
+KOGEKKO_DOWNLOADS="${out_dir}" docker compose run --rm ko_gekko \
+    pipenv run python src/main.py "$@"
 
+# Check if we downloaded something
 downloaded_files=$(find "${out_dir}" -mindepth 1 -maxdepth 1 -type f,d | wc -l)
 
-echo "${out_dir}"
-find "${out_dir}" -mindepth 1 -maxdepth 1 -type f,d
-
+# In case we downloaded things let's copy them to the local download folder,
+# fixing permissions as well
 if ((downloaded_files > 0)); then
-    final_download_dir="${runner_dir}/ko_gekko_downloads/"
-    cp -r "${out_dir}" "${final_download_dir}" || exit
+    final_download_dir="${runner_dir}/ko_gekko_downloads"
+    cp -rfT "${out_dir}" "${final_download_dir}" || exit
 fi
